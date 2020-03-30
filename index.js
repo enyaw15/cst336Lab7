e751b644d3192872b591747be8a3ffd117b209df
Wayne <enyaw0215@gmail.com>
2020-03-30 01:32:25 +0000

    added pixabay search and return

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");//import express
 var app = express();//create an app
+var request = require("request");
 
 
 //tells the app to use the public folder
@@ -16,10 +17,42 @@ app.listen(process.env.PORT, function()
 //looks for url matching the string and does the function
 // looks for a /
 app.get("/", function(req,res){
-    res.render("home.ejs")
+    var url = `https://pixabay.com/api/?key=15401070-3f0e62fc6ef63c9390f6fb50d&safesearch=true}`
+    request(url,function(error,response,dataStream){
+        if(!error && response.statusCode == 200){
+            var json = JSON.parse(dataStream);
+            var images = selectFourImages(json);
+            res.render('results',{keyword:"flower",images:images});
+        }
+    })
+});
+
+app.get("/search", function(req, res)
+{
+    var keyword = req.query.keyword;
+    var orientation = req.query.orientation == "h"? "horizontal" : "vertical";
+    var url = `https://pixabay.com/api/?key=15401070-3f0e62fc6ef63c9390f6fb50d&q=${keyword}&orientation=${orientation}&safesearch=true}`;
+    request(url,function(error,response,dataStream){
+        if(!error && response.statusCode == 200){
+            var json = JSON.parse(dataStream);
+            var images = selectFourImages(json);
+            res.render('results',{keyword:keyword,images:images});
+        }
+    });
 });
 
 // regex star takes everything this should be last so that other options are hit first
 app.get("*", function(req, res){
     res.render("error.ejs");
 });
+
+function selectFourImages(data)
+{
+    let images = [];
+    for(var i = 0; i < 4; i++)
+    {
+        let img = data.hits[Math.floor(Math.random()*data.hits.length)];
+        images.push(img);
+    }
+    return images;
+}
